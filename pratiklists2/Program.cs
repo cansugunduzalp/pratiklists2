50b320a534528bec80e8fd5bbf0b7203a1fc38cc
cansugunduzalp <cansugunduzalp@gmail.com>
2025-01-26 00:35:27 +0300

    Add project files.

---
+++ pratiklists2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> kahveler = new List<string>();
+
+        // Kullanıcıdan 5 kahve ismi alıyoruz
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"Kahve {i}:");
+            string kahveIsmi = Console.ReadLine();
+            kahveler.Add(kahveIsmi);
+        }
+
+        // Listeyi foreach döngüsü ile yazdırıyoruz
+        Console.WriteLine("\nGirdiğiniz kahve isimleri:");
+        foreach (string kahve in kahveler)
+        {
+            Console.WriteLine(kahve);
+        }
+    }
+}
